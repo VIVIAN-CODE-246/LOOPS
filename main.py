55e0022328178b5cff0def7353d061c41b1a3574
VIVIAN-CODE-246 <zaraconcept247@gmail.com>
2025-01-12 13:44:07 +0200

    LOOPS

---
+++ main.py
@@ -0,0 +1,17 @@
+for x in range(1,11):
+    print(x * 50)
+
+for character in "VIVIAN":
+    print(character)
+
+foods = ["RICE", "BEANS", "YAM"]
+adj = ["carbohydrate", "protein", "carbohydrate"]
+
+for m in foods:
+    print()
+    for y in adj:
+       print(y,m)
+
+countries = ["Nigeria", "Ghana", "Kenya", "America", "Rwanda"]
+for x in countries:
+    print(x)
